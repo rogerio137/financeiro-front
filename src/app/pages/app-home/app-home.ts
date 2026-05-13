85d5d0a6ed8407a66d3ebea994bd0af6682cce01
Rogerio <barrucalleortiz@gmail.com>
2026-05-12 21:56:25 -0300

    Alteração no componente de pagina inicial

--- src/app/pages/app-home/app-home.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-app-home',
-  imports: [],
-  templateUrl: './app-home.html',
-  styleUrl: './app-home.scss',
-})
-export class AppHome {}
--- src/app/pages/home-component/home-component.spec.ts
+++ src/app/pages/home-component/home-component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppHome } from './app-home';
+import { HomeComponent } from './home-component';
 
-describe('AppHome', () => {
-  let component: AppHome;
-  let fixture: ComponentFixture<AppHome>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppHome],
+      imports: [HomeComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(AppHome);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
