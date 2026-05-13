17b165a6b661c6a332b52ea07f221ec309776cdc
Rogerio <barrucalleortiz@gmail.com>
2026-05-12 22:41:32 -0300

    Criação de menu lateral

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,8 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home-component/home-component";
 
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HomeComponent],
--- src/app/pages/home-component/home-component.ts
+++ src/app/pages/home-component/home-component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  imports: [MatSidenavModule, MatButtonModule],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
 })
