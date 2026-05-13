b67b0c5474a829ac9e71dd84ad0977c8c572937e
Rogerio <barrucalleortiz@gmail.com>
2026-05-12 21:47:41 -0300

    Criação do componente de pagina inicial

---
+++ src/app/pages/app-home/app-home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-home',
+  imports: [],
+  templateUrl: './app-home.html',
+  styleUrl: './app-home.scss',
+})
+export class AppHome {}
