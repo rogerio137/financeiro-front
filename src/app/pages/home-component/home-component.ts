8fca46d7d845b717686280ef0ecf3db79ac73558
Rogerio <barrucalleortiz@gmail.com>
2026-05-14 20:49:00 -0300

    Criação dos links do menu lateral

--- src/app/pages/home-component/home-component.ts
+++ src/app/pages/home-component/home-component.ts
@@ -4,10 +4,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDrawer } from '@angular/material/sidenav';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home-component',
-  imports: [MatSidenavModule, MatButtonModule, MatDrawer, MatToolbar, MatIcon],
+  imports: [MatSidenavModule, 
+            MatButtonModule, 
+            MatDrawer, 
+            MatToolbar, 
+            MatIcon, 
+            MatListModule,
+            TitleCasePipe],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
 })
@@ -17,4 +25,7 @@ export class HomeComponent {
   toggleDrawer() {
     this.drawer.toggle();
   }
+
+  fragments = ['inbox', 'outbox', 'drafts'];
+  activeLink: string | null = null;
 }
