7b521aeb5c3536ad21692eb819095350f308423e
Rogerio <barrucalleortiz@gmail.com>
2026-05-14 16:55:24 -0300

    Ajuste no botão de abrir o menu

--- src/app/pages/home-component/home-component.ts
+++ src/app/pages/home-component/home-component.ts
@@ -2,10 +2,12 @@ import { Component, ViewChild  } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDrawer } from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-component',
-  imports: [MatSidenavModule, MatButtonModule, MatDrawer],
+  imports: [MatSidenavModule, MatButtonModule, MatDrawer, MatToolbar, MatIcon],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
 })
