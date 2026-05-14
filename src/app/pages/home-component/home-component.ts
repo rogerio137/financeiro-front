20738e4708f145ccafb450910f4b7f4a0547a41d
Rogerio <barrucalleortiz@gmail.com>
2026-05-14 16:24:13 -0300

    Incluisão do efeito de esconder o menu

--- src/app/pages/home-component/home-component.ts
+++ src/app/pages/home-component/home-component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-home-component',
-  imports: [MatSidenavModule, MatButtonModule],
+  imports: [MatSidenavModule, MatButtonModule, MatDrawer],
   templateUrl: './home-component.html',
   styleUrl: './home-component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  @ViewChild('drawer') drawer!: MatDrawer;
+
+  toggleDrawer() {
+    this.drawer.toggle();
+  }
+}
