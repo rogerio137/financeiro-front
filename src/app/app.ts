bdd8166b564c02d9ddd191eebba7445b02f92faa
Rogerio <barrucalleortiz@gmail.com>
2026-05-14 22:54:37 -0300

    Inclusão dos campos de cartão

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CartaoComponent } from './pages/cartao-component/cartao-component';
+import { HomeComponent } from './pages/home-component/home-component';
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CartaoComponent],
+  imports: [RouterOutlet, CartaoComponent, HomeComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
