5b5713719f9dcce939c62f2f0a726661ecc385ff
Rogerio <barrucalleortiz@gmail.com>
2026-05-15 12:04:46 -0300

    Ajustes no layout de cadastro de cartões

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { HomeComponent } from './pages/home-component/home-component';
+import { CartaoComponent } from "./pages/cartao-component/cartao-component";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [HomeComponent, CartaoComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
